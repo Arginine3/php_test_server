02631f94ecb39bfbb039da787a75d5eeb206bd7a
Arginine3 <ym.love.1003@icloud.com>
2021-12-11 16:01:07 +0900

    Seederを使ったダミーデータを作成した

---
+++ src/database/migrations/2021_12_11_051149_create_weight_logs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWeightLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weight_logs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+			$table->string("date_key"); //日付キー
+			$table->float("weight");	//体重
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weight_logs');
+    }
+}
